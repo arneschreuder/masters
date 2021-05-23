e999368a262379d744a896fd66cdc634a1ce9a20
Arné Schreuder <arneschreuder@icloud.com>
2021-05-23 20:38:51 +0200

    📝 (framework): Added datasets documentation

--- framework/datasets/dataset.py
+++ framework/datasets/dataset.py
@@ -1,29 +1,82 @@
+# MIT License
+
+# Copyright 2021 Arné Schreuder
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from typing import List
 
 import tensorflow as tf
 
 
 class Dataset:
-    features: List[str] = None
-    label: str = None
+    """
+    The base class used for datasets
+
+    Attributes
+    -----------
+    batch_size: int
+        Specifies the mini-batch size. Default = None
+    classes: List[str]
+        A list of strings that contain the different classes. Default = None
+    columns: List[str]
+        A list of strings that contain the different column headings. Default = None
+    features: List[str]
+        A list of strings that contain the different features' headings. Default = None
+    label: str
+        The column heading of the label column. Default = None
+    seed: int
+        Random seed value. Default = None
+    shuffle_size: int
+        Specifies the number of items to include in shuffle process. Default = None
+    test: tf.data.Dataset
+        Contains the test dataset. Default = None
+    training: tf.data.Dataset
+        Contains the training dataset. Default = None
+    validation: tf.data.Dataset
+        Contains the validation dataset. Default = None
+    """
+    batch_size: int = None
     classes: List[str] = None
     columns: List[str] = None
-    shuffle_sizel: int = None
-    batch_size: int = None
-
+    features: List[str] = None
+    label: str = None
     seed: int = None
+    shuffle_size: int = None
+    test: tf.data.Dataset = None
     training: tf.data.Dataset = None
     validation: tf.data.Dataset = None
-    test: tf.data.Dataset = None
 
-    def __init__(self, seed=None):
-        self.features = None
-        self.label = None
+    def __init__(self, seed: int = None):
+        """
+        Params
+        ------
+        seed: int
+            Random seed value. Default = None
+        """
+        self.batch_size = None
         self.classes = None
         self.columns = None
-        self.shuffle_sizel = None
-        self.batch_size = None
+        self.features = None
+        self.label = None
         self.seed = seed
+        self.shuffle_size = None
+        self.test = None
         self.training = None
         self.validation = None
-        self.test = None
--- framework/datasets/iris/iris.py
+++ framework/datasets/iris/iris.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright 2021 Arné Schreuder
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import os
 
 import pandas as pd
@@ -7,9 +29,20 @@ from framework.datasets.dataset import Dataset
 
 
 class Iris(Dataset):
-    def __init__(self, seed=None):
-        super(Iris, self).__init__(seed=seed)
+    """
+    The implementation of the Iris dataset
+    """
 
+    def __init__(self, seed: int = None):
+        """
+        Params
+        ------
+        seed: int
+            Random seed value. Default = None
+        """
+        super(Iris, self).__init__(seed=seed)
+        self.classes = ["setosa", "versicolor", "virginica"]
+        self.columns = self.features + [self.label]
         self.features = [
             "sepal_length",
             "sepal_width",
@@ -17,10 +50,8 @@ class Iris(Dataset):
             "petal_width"
         ]
         self.label = "species"
-        self.classes = ["setosa", "versicolor", "virginica"]
-        self.columns = self.features + [self.label]
-        self.shuffle_size = 150
         self.batch_size = 30
+        self.shuffle_size = 150
 
         directory = os.path.dirname(os.path.abspath(__file__))
         file = "iris.csv"
