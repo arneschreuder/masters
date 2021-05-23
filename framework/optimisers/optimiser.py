9ff2d92701ccf3310e777399d7ab0c6c6e7b9719
Arné Schreuder <arneschreuder@icloud.com>
2021-05-23 23:00:16 +0200

    📝 (framework): Optimisers documentation.

--- framework/optimisers/optimiser.py
+++ framework/optimisers/optimiser.py
@@ -1,3 +1,29 @@
+# The MIT License (MIT)
+# =====================
+
+# Copyright 2021 Arné Schreuder
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the “Software”), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 from typing import Tuple
 
 import tensorflow as tf
@@ -6,6 +32,18 @@ from framework.neural_networks.neural_network import NeuralNetwork
 
 
 class Optimiser:
+    """
+    The base class for all implementations of an optimiser.
+    Optimisers are essentially stateful instances that execute
+    a specific heuristic over its own state.
+
+    Attributes
+    ----------
+    loss_fn: Loss
+        The loss function to use. Default = None
+    model: NeuralNetwork
+        The model to be trained. Default = None
+    """
     loss_fn: Loss = None
     model: NeuralNetwork = None
 
@@ -14,15 +52,51 @@ class Optimiser:
         self.model = None
 
     def set_loss_fn(self, loss_fn: Loss) -> None:
+        """
+        Sets the loss function
+
+        Parameters
+        ----------
+        loss_fn: Loss
+            The loss function to use. Default = None
+        """
         self.loss_fn = loss_fn
 
     def set_model(self, model: NeuralNetwork) -> None:
+        """
+        Sets the loss function
+
+        Parameters
+        ----------
+        model: NeuralNetwork
+        The model to be trained. Default = None
+        """
         self.model = model
 
     def initialise(self) -> None:
+        """
+        Initialisers function. Initialises the model
+        """
         self.model.initialise()
 
-    def evaluate(self, features: tf.Tensor, labels: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+    def evaluate(self,
+                 features: tf.Tensor,
+                 labels: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+        """
+        A single evaluation of the contained model against the loss function.
+
+        Parameters
+        ----------
+        features: tf.Tensor
+            The input data
+        labels: tf.Tensor
+            The target data/labels
+
+        Returns
+        -------
+        Tuple[tf.Tensor, tf.Tensor]
+            Consists out of (logits, loss)
+        """
         # Evaluate model
         logits = self.model(features=features)
         loss = tf.reduce_mean(
@@ -32,6 +106,3 @@ class Optimiser:
             )
         )
         return logits, loss
-
-    def __call__(self, features: tf.Tensor, labels: tf.Tensor):
-        pass
--- framework/optimisers/sgd.py
+++ framework/optimisers/sgd.py
@@ -1,3 +1,29 @@
+# The MIT License (MIT)
+# =====================
+
+# Copyright 2021 Arné Schreuder
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the “Software”), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 from typing import List, Tuple
 
 import tensorflow as tf
@@ -7,8 +33,24 @@ from framework.utilities.utilities import flatten, reshape
 
 
 class SGD(Optimiser):
-    learning_rate: float = None,
-    momentum: float = None,
+    """
+    The Stochastic Gradient Descent concrete optimiser.
+
+    Attributes
+    ----------
+    learning_rate: float
+        The step size. Default = None
+    momentum: float
+        Momentum hyper-heuristic. Default = None
+    nesterov: bool
+        Flag to use nesterov update rule. Default = None
+    entity: Entity
+        The entity that represents the candidate solution to the model.
+        Default = None
+
+    """
+    learning_rate: float = None
+    momentum: float = None
     nesterov: bool = None
     entity: Entity = None
 
@@ -16,6 +58,16 @@ class SGD(Optimiser):
                  learning_rate: float = 0.01,
                  momentum: float = 0.9,
                  nesterov: bool = True):
+        """
+        Parameters
+        ----------
+        learning_rate: float
+            The step size. Default = None
+        momentum: float
+            Momentum hyper-heuristic. Default = None
+        nesterov: bool
+            Flag to use nesterov update rule. Default = None
+        """
         super(SGD, self).__init__()
         self.learning_rate: float = learning_rate,
         self.momentum: float = momentum,
@@ -23,12 +75,34 @@ class SGD(Optimiser):
         self.entity: Entity = None
 
     def initialise(self) -> None:
+        """
+        Initialiser function.
+        Creates the entity, maps the model and initialises the entity.
+        """
         super(SGD, self).initialise()
         self.entity = Entity()
+        # This is required to determine the dimensionality of the model.
         self.entity.map_model(model=self.model)
         self.entity.initialise()
 
-    def get_gradient(self, features: tf.Tensor, labels: tf.Tensor) -> List[List[tf.Tensor]]:
+    def get_gradient(self,
+                     features: tf.Tensor,
+                     labels: tf.Tensor) -> List[List[tf.Tensor]]:
+        """
+        Calculates the gradient of the model parameters based on loss function.
+
+        Parameters
+        ----------
+        features: tf.Tensor
+            The input data
+        labels: tf.Tensor
+            The target data/labels
+
+        Returns
+        -------
+        List[List[tf.Tensor]]
+            The gradient as a list of list of tensors
+        """
         with tf.GradientTape() as tape:
             parameters = self.model.get_parameters()
             tape.watch(parameters)
@@ -42,7 +116,22 @@ class SGD(Optimiser):
             )
             return gradient
 
-    def step(self, position: tf.Tensor, velocity: tf.Tensor, gradient: tf.Tensor) -> None:
+    def step(self,
+             position: tf.Tensor,
+             velocity: tf.Tensor,
+             gradient: tf.Tensor) -> None:
+        """
+        The heuristic step operation.
+
+        Parameters
+        ----------
+        position: tf.Tensor
+            The entity's position which is the candidate solution to the model
+        velocity: tf.Tensor
+            The entity's velocity
+        gradient: tf.Tensor
+            The gradient to apply
+        """
         if self.momentum == 0.0:
             position.assign_add(-self.learning_rate*gradient)
         else:
@@ -57,10 +146,28 @@ class SGD(Optimiser):
             else:
                 position.assign_add(velocity)
 
-    def __call__(self, features: tf.Tensor, labels: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+    def __call__(self,
+                 features: tf.Tensor,
+                 labels: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+        """
+        A single execution of the optimiser's step.
+
+        Parameters
+        ----------
+        features: tf.Tensor
+            The input data
+        labels: tf.Tensor
+            The target data/labels
+
+        Returns
+        -------
+        Tuple[tf.Tensor, tf.Tensor]
+            Consists out of (logits, loss)
+        """
         # Load model with solution
         self.model.set_parameters_flat(parameters_flat=self.entity.position)
 
+        # Get gradients
         gradient = self.get_gradient(features=features, labels=labels)
         gradient_flat = flatten(parameters=gradient)
 
