90a6441c95b972324f2130991e34778a979374c0
Arné Schreuder <arneschreuder@icloud.com>
2021-05-26 18:23:12 +0200

    🎨 (framework): Reworking naming of variables

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import datetime
 import framework as fw
 
 experiment = fw.experiments.Iris(
-    optimiser=fw.optimisers.SGD(),
+    optimiser=fw.optimisers.PSO(
+        population_size=10,
+    ),
     log_dir="logs",
     seed=1
 )
 
 if __name__ == "__main__":
     experiment.initialise()
-    # experiment()
+    experiment()
