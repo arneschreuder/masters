c11942788145c00309842fc3333e4e65b4e86e6d
Arné Schreuder <arneschreuder@icloud.com>
2021-12-01 16:13:48 +0200

    ✨ (experiments): More memory

--- experiments/chpc/chpc_bhh_variant_normalise.sh
+++ experiments/chpc/chpc_bhh_variant_normalise.sh
@@ -3,7 +3,7 @@
 #PBS -N 5_bhh_variant_normalise
 #PBS -q serial
 #PBS -P CSCI0886
-#PBS -l select=1:ncpus=24:mem=64gb:ngpus=0
+#PBS -l select=1:ncpus=24:mem=128gb:ngpus=0
 #PBS -l walltime=48:00:00
 #PBS -o /mnt/lustre/users/aschreuder/5_stdoutput.out
 #PBS -e /mnt/lustre/users/aschreuder/5_stderror.err
--- experiments/chpc/chpc_bhh_variant_reselection.sh
+++ experiments/chpc/chpc_bhh_variant_reselection.sh
@@ -3,7 +3,7 @@
 #PBS -N 9_bhh_variant_reselection
 #PBS -q serial
 #PBS -P CSCI0886
-#PBS -l select=1:ncpus=24:mem=64gb:ngpus=0
+#PBS -l select=1:ncpus=24:mem=128gb:ngpus=0
 #PBS -l walltime=48:00:00
 #PBS -o /mnt/lustre/users/aschreuder/9_stdoutput.out
 #PBS -e /mnt/lustre/users/aschreuder/9_stderror.err
