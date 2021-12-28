333fcbf588e4849e515a67dd0ddaaaba59bb9d3a
Arné Schreuder <arneschreuder@icloud.com>
2021-12-28 22:19:00 +0200

    ✨ (experiments): Moving to smp queue

--- experiments/chpc/chpc_bhh_gd_only_variant_reanalysis.sh
+++ experiments/chpc/chpc_bhh_gd_only_variant_reanalysis.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #PBS -N 17_bhh_gd_only_variant_reanalysis
-#PBS -q normal
+#PBS -q smp
 #PBS -P CSCI0886
 #PBS -l select=1:ncpus=24:mem=120gb:ngpus=0
-#PBS -l walltime=48:00:00
+#PBS -l walltime=96:00:00
 #PBS -o /mnt/lustre/users/aschreuder/revised/17_stdoutput.out
 #PBS -e /mnt/lustre/users/aschreuder/revised/17_stderror.err
 #PBS -m abe -M arneschreuder@icloud.com
