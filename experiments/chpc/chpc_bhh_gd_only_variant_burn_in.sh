7bd07009f399b6819bf685b7efb4d5af51c3efb4
Arné Schreuder <arneschreuder@icloud.com>
2021-12-28 21:25:59 +0200

    🔧 (config): Path changes to scripts

--- experiments/chpc/chpc_bhh_gd_only_variant_burn_in.sh
+++ experiments/chpc/chpc_bhh_gd_only_variant_burn_in.sh
@@ -5,14 +5,14 @@
 #PBS -P CSCI0886
 #PBS -l select=1:ncpus=24:mem=120gb:ngpus=0
 #PBS -l walltime=48:00:00
-#PBS -o /mnt/lustre/users/aschreuder/12_stdoutput.out
-#PBS -e /mnt/lustre/users/aschreuder/12_stderror.err
+#PBS -o /mnt/lustre/users/aschreuder/revised/12_stdoutput.out
+#PBS -e /mnt/lustre/users/aschreuder/revised/12_stderror.err
 #PBS -m abe -M arneschreuder@icloud.com
 
 ulimit -s unlimited
 
 # Move to the directory of interest
-cd /mnt/lustre/users/aschreuder/masters.ai/
+cd /mnt/lustre/users/aschreuder/revised/masters.ai/
 
 # Can activate your python environment here
 module purge
